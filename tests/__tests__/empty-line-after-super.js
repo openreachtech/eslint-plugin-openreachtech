c4f7383cefeb5f43231f16a2385183e01e9a76e3
lisadhl <907629413@qq.com>
2022-04-11 22:43:14 +0900

    Create rule empty-line-after-super: refactor unit test.

--- tests/__tests__/empty-line-after-super.js
+++ tests/__tests__/empty-line-after-super.js
@@ -5,347 +5,10 @@
 // Requirements
 //  ------------------------------------------------------------------------------
 
-const { RuleTester } = require('eslint/lib/rule-tester')
+// ESLint tester instead of Jest `test()`
+const tester = require('../tools/ESLintHelper').createTester()
 
 const rule = require('../../lib/empty-line-after-super')
-
-const validCodes = [
-  // has no statements after super()
-  `
-  class A extends B {
-    constructor (name) { 
-      super(name)
-    } 
-  }
-  `,
-
-  // has empty line after super()
-  `
-    class A extends B {
-      constructor (name, age) {
-        super(name)
- 
-        this.age = age
-      }
-    }
-  `,
-
-  // has empty lines after super()
-  `
-    class A extends B {
-      constructor (name, age) {
-        super(name)
-        
- 
-        this.age = age
-      }
-    }
-  `,
-
-  // has empty line after super() - single line comments
-  `
-   class A extends B {
-     constructor (name, age) {
-       super(name) // does not require age.
-
-       this.age = age
-     }
-   }
- `,
-
-  // has empty line after super() - single line comments at new line
-  `
-   class A extends B {
-     constructor (name, age) {
-       super(name)
-
-       // NOTE: Max number of age is 99.
-       this.age = age
-     }
-   }
- `,
-
-  // has empty line after super() - multiple lines comments
-  `
-   class A extends B {
-     constructor (name, age) {
-       super(name)
-        
-       /**
-        * @type {{
-        *   age: number,
-        *   name: null, // default name
-        * }}
-        */
-       this.age = age
-     }
-   }
- `,
-
-  // has empty line after super() - super call break to multiple lines
-  `
-  class A extends B {
-    constructor (name, age) {
-      super(
-        name
-      )
-
-      this.age = age
-    }
-  }
-  `,
-]
-
-const invalidCodes = [
-  // has no empty line after super() - multiple statements in same line
-  {
-    code: `
-      class A extends B {
-        constructor (name, age) {
-          super(name); this.age = age
-        }
-      }
-    `,
-    output: `
-      class A extends B {
-        constructor (name, age) {
-          super(name);
-
-          this.age = age
-        }
-      }
-    `
-  },
-
-  // has no empty line after super()
-  {
-    code: `
-      class A extends B {
-        constructor (name, age) {
-          super(name)
-          this.age = age
-        }
-      }
-    `,
-    output: `
-      class A extends B {
-        constructor (name, age) {
-          super(name)
-
-          this.age = age
-        }
-      }
-    `
-  },
-
-  // has no empty line after super() - super call break to multiple lines
-  {
-    code: `
-      class A extends B {
-        constructor (name, age) {
-          super(
-            name
-          )
-          this.age = age
-        }
-      }
-    `,
-    output: `
-      class A extends B {
-        constructor (name, age) {
-          super(
-            name
-          )
-
-          this.age = age
-        }
-      }
-    `
-  },
-
-  // has no empty line after super() - single line comment
-  {
-    code: `
-      class A extends B {
-        constructor (name, age) {
-          super(name) // does not require age.
-          this.age = age
-        }
-      }
-    `,
-    output: `
-      class A extends B {
-        constructor (name, age) {
-          super(name) // does not require age.
-
-          this.age = age
-        }
-      }
-    `
-  },
-
-  // has no empty line after super() - single line comment in new line
-  {
-    code: `
-      class A extends B {
-        constructor (name, age) {
-          super(name)
-          // NOTE: Max number of age is 99.
-          this.age = age
-        }
-      }
-    `,
-    output: `
-      class A extends B {
-        constructor (name, age) {
-          super(name)
-
-          // NOTE: Max number of age is 99.
-          this.age = age
-        }
-      }
-    `
-  },
-
-  // has no empty line after super() - multiple lines comments
-  {
-    code: `
-      class A extends B {
-        constructor (name, age) {
-          super(name)
-          /**
-           * @type {{
-           *   age: number,
-           *   name: null, // default name
-           * }}
-           */
-          this.age = age
-        }
-      }
-    `,
-    output: `
-      class A extends B {
-        constructor (name, age) {
-          super(name)
-
-          /**
-           * @type {{
-           *   age: number,
-           *   name: null, // default name
-           * }}
-           */
-          this.age = age
-        }
-      }
-    `
-  },
-
-  // has no empty line after super() - multiple lines comments start from super line
-  {
-    code: `
-      class A extends B {
-        constructor (name, age) {
-          super(name) /**
-           * @type {{
-           *   age: number,
-           *   name: null, // default name
-           * }}
-           */
-          this.age = age
-        }
-      }
-    `,
-    output: `
-      class A extends B {
-        constructor (name, age) {
-          super(name)
-
-          /**
-           * @type {{
-           *   age: number,
-           *   name: null, // default name
-           * }}
-           */
-          this.age = age
-        }
-      }
-    `
-  },
-
-  // has no empty line after super() - comments before super
-  {
-    code: `
-      class A extends B {
-        constructor (name, age) {
-          /* age is default */ /* name is default */ super(name)
-          this.age = age
-        }
-      }
-    `,
-    output: `
-      class A extends B {
-        constructor (name, age) {
-          /* age is default */ /* name is default */ super(name)
-
-          this.age = age
-        }
-      }
-    `
-  },
-
-  // has no empty line after super() -  multiple lines comments before super
-  {
-    code: `
-      class A extends B {
-        constructor (name, age) {
-          /* age is default  
-           * name is default 
-           */ super(name)
-          this.age = age
-        }
-      }
-    `,
-    output: `
-      class A extends B {
-        constructor (name, age) {
-          /* age is default  
-           * name is default 
-           */ super(name)
-
-          this.age = age
-        }
-      }
-    `
-  },
-
-  // has no empty line after super() - all in one line
-  {
-    code: `
-      class A extends B {constructor (name, age) {super(name); this.age = age;}}
-    `,
-    output: `
-      class A extends B {constructor (name, age) {super(name);
-
-      this.age = age;}}
-    `
-  },
-
-  // has no empty line after super() - all in one line in constructor
-  {
-    code: `
-      class A extends B {
-        constructor (name, age) {super(name); this.age = age;}
-      }
-    `,
-    output: `
-      class A extends B {
-        constructor (name, age) {super(name);
-
-        this.age = age;}
-      }
-    `
-  },
-]
 
 const errors = [{
   messageId: 'errorMessage',
@@ -353,13 +16,374 @@ const errors = [{
   type: 'ExpressionStatement'
 }]
 
+const name = 'empty-line-after-super'
+
 // ------------------------------------------------------------------------------
 // Tests
 //  ------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } })
+describe('Require empty line between super call and other statements.', () => {
+  describe('valid', () => {
+    const validCodes = [
+      {
+        description: 'has no statements after super()',
+        code:`
+          class A extends B {
+            constructor (name) {
+              super(name)
+            } 
+          }
+        `
+      },
+      {
+        description: 'has empty line after super()',
+        code:`
+          class A extends B {
+            constructor (name, age) {
+              super(name)
+     
+              this.age = age
+            }
+          }
+        `
+      },
+      {
+        description: 'has empty lines after super()',
+        code:`
+          class A extends B {
+            constructor (name, age) {
+              super(name)
+            
+     
+              this.age = age
+            }
+          }
+        `
+      },
+      {
+        description: 'has empty line after super() - single line comments',
+        code:`
+          class A extends B {
+            constructor (name, age) {
+              super(name) // does not require age.
+ 
+              this.age = age
+            }
+          }
+        `
+      },
+      {
+        description: 'has empty line after super() - single line comments at new line',
+        code:`
+          class A extends B {
+            constructor (name, age) {
+              super(name)
+     
+              // NOTE: Max number of age is 99.
+              this.age = age
+            }
+          }
+        `
+      },
+      {
+        description: 'has empty line after super() - multiple lines comments',
+        code:`
+          class A extends B {
+            constructor (name, age) {
+              super(name)
+             
+              /**
+               * @type {{
+               *   age: number,
+               *   name: null, // default name
+               * }}
+               */
+              this.age = age
+            }
+          }
+        `
+      },
+      {
+        description: 'has empty line after super() - super call break to multiple lines',
+        code:`
+          class A extends B {
+            constructor (name, age) {
+              super(
+                name
+              )
+      
+              this.age = age
+            }
+          }
+        `
+      },
+    ]
 
-ruleTester.run('empty-line-after-super', rule, {
-  valid: validCodes.map(code => ({ code })),
-  invalid: invalidCodes.map(({ code, output }) => ({ code, output, errors }))
+    describe.each(validCodes)('$# - $description', ({ code }) => {
+      tester.run(
+        name,
+        rule,
+        {
+          valid: [{ code }],
+          invalid: []
+        }
+      )
+    })
+  })
+
+  describe('invalid', () => {
+    const invalidCodes = [
+      {
+        description: 'has no empty line after super() - multiple statements in same line',
+        code: `
+          class A extends B {
+            constructor (name, age) {
+              super(name); this.age = age
+            }
+          }
+        `,
+        output: `
+          class A extends B {
+            constructor (name, age) {
+              super(name);
+
+              this.age = age
+            }
+          }
+        `
+      },
+      {
+        description: 'has no empty line after super()',
+        code: `
+          class A extends B {
+            constructor (name, age) {
+              super(name)
+              this.age = age
+            }
+          }
+        `,
+        output: `
+          class A extends B {
+            constructor (name, age) {
+              super(name)
+
+              this.age = age
+            }
+          }
+        `
+      },
+      {
+        description: 'has no empty line after super() - super call break to multiple lines',
+        code: `
+          class A extends B {
+            constructor (name, age) {
+              super(
+                name
+              )
+              this.age = age
+            }
+          }
+        `,
+        output: `
+          class A extends B {
+            constructor (name, age) {
+              super(
+                name
+              )
+
+              this.age = age
+            }
+          }
+        `
+      },
+      {
+        description: 'has no empty line after super() - single line comment',
+        code: `
+          class A extends B {
+            constructor (name, age) {
+              super(name) // does not require age.
+              this.age = age
+            }
+          }
+        `,
+        output: `
+          class A extends B {
+            constructor (name, age) {
+              super(name) // does not require age.
+
+              this.age = age
+            }
+          }
+        `
+      },
+      {
+        description: 'has no empty line after super() - single line comment in new line',
+        code: `
+          class A extends B {
+            constructor (name, age) {
+              super(name)
+              // NOTE: Max number of age is 99.
+              this.age = age
+            }
+          }
+        `,
+        output: `
+          class A extends B {
+            constructor (name, age) {
+              super(name)
+
+              // NOTE: Max number of age is 99.
+              this.age = age
+            }
+          }
+        `
+      },
+      {
+        description: 'has no empty line after super() - multiple lines comments',
+        code: `
+          class A extends B {
+            constructor (name, age) {
+              super(name)
+              /**
+               * @type {{
+               *   age: number,
+               *   name: null, // default name
+               * }}
+               */
+              this.age = age
+            }
+          }
+        `,
+        output: `
+          class A extends B {
+            constructor (name, age) {
+              super(name)
+
+              /**
+               * @type {{
+               *   age: number,
+               *   name: null, // default name
+               * }}
+               */
+              this.age = age
+            }
+          }
+        `
+      },
+      {
+        description: 'has no empty line after super() - multiple lines comments start from super line',
+        code: `
+          class A extends B {
+            constructor (name, age) {
+              super(name) /**
+               * @type {{
+               *   age: number,
+               *   name: null, // default name
+               * }}
+               */
+              this.age = age
+            }
+          }
+        `,
+        output: `
+          class A extends B {
+            constructor (name, age) {
+              super(name)
+
+              /**
+               * @type {{
+               *   age: number,
+               *   name: null, // default name
+               * }}
+               */
+              this.age = age
+            }
+          }
+        `
+
+      },
+      {
+        description: 'has no empty line after super() - comments before super',
+        code: `
+          class A extends B {
+            constructor (name, age) {
+              /* age is default */ /* name is default */ super(name)
+              this.age = age
+            }
+          }
+        `,
+        output: `
+          class A extends B {
+            constructor (name, age) {
+              /* age is default */ /* name is default */ super(name)
+
+              this.age = age
+            }
+          }
+        `
+
+      },
+      {
+        description: 'has no empty line after super() -  multiple lines comments before super',
+        code: `
+          class A extends B {
+            constructor (name, age) {
+              /* age is default
+               * name is default
+               */ super(name)
+              this.age = age
+            }
+          }
+        `,
+        output: `
+          class A extends B {
+            constructor (name, age) {
+              /* age is default
+               * name is default
+               */ super(name)
+
+              this.age = age
+            }
+          }
+        `
+      },
+      {
+        description: 'has no empty line after super() - all in one line',
+        code: `
+          class A extends B {constructor (name, age) {super(name); this.age = age;}}
+        `,
+        output: `
+          class A extends B {constructor (name, age) {super(name);
+
+          this.age = age;}}
+        `
+      },
+      {
+        description: 'has no empty line after super() - all in one line in constructor',
+        code: `
+          class A extends B {
+            constructor (name, age) {super(name); this.age = age;}
+          }
+        `,
+        output: `
+          class A extends B {
+            constructor (name, age) {super(name);
+
+            this.age = age;}
+          }
+        `
+      },
+    ]
+
+    describe.each(invalidCodes)('$# - $description', ({ code, output }) => {
+      tester.run(
+        name,
+        rule,
+        {
+          valid: [],
+          invalid: [{ code, output, errors }]
+        }
+      )
+    })
+  })
 })
