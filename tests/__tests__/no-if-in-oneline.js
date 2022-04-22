9dc6c81f2bf5f3e2d9856cc062bdb896b785fda1
lisadhl <907629413@qq.com>
2022-04-23 08:41:24 +0900

    Implement eslint rule no-if-in-oneline - else branch.

--- tests/__tests__/no-if-in-oneline.js
+++ tests/__tests__/no-if-in-oneline.js
@@ -6,89 +6,171 @@ const RuleTester = require('eslint').RuleTester
 
 const noIfInOnelineRule = require('../../lib/no-if-in-oneline')
 
-const errors = ['Forbid if statements to be written on a single line.']
-const validCodes = [
-  `
-    if (condition);
-  `,
-  `
-	  if (condition) {}
-  `,
-  `
-    function test() {
-      if (condition) {
-        return
-      }
-    }
-  `,
-  `
-    function test() {
-      if (condition) {
-        return
-      }
-    }
-  `,
-  `
-    if (condition) {
-      foo()
-      bar()
-    }
-  `,
-  `
-    function test() {
-      if (condition
-        ||condition2
-      ) {
-        return
-      }
-    }
-  `,
-]
-const invalidCodes = [
-  `
-    function test() {
-      if (condition) return
-    }
-  `,
-  `
-    function test() {
-      if (condition) { return }
-    }
-  `,
-  `
-    function test() {
-      if (condition) { foo()
-        return
-      }
-    }
-  `,
-  `
-    function test() {
-      if (condition) { m
-        .foo()
-        .bar
-
-        return
-      }
-    }
-  `,
-  `
-    function test() {
-      if (condition
-        || condition2
-      ) { return }
-    }
-  `,
-]
-
-describe('Forbid if statements to be written on a single line.', () => {
+describe('Forbid if statements and else to be written on a single line.', () => {
+  const errors = ['Forbid if statements to be written on a single line.']
+  const name = 'no-if-in-oneline'
   const tester = new RuleTester()
-  tester.run(
-    'no-if-in-oneline',
-    noIfInOnelineRule,
-    {
-      valid: validCodes.map(code => ({ code })),
-      invalid: invalidCodes.map(code => ({ code, errors }))
-    }
-  )
+
+  describe('if branch', () => {
+    const validCodes = [
+      `
+        if (condition);
+      `,
+      `
+        if (condition) {}
+      `,
+      `
+        function test() {
+          if (condition) {
+            return
+          }
+        }
+      `,
+      `
+        if (condition) {
+          foo()
+          bar()
+        }
+      `,
+      `
+        function test() {
+          if (condition
+            ||condition2
+          ) {
+            return
+          }
+        }
+      `,
+    ]
+    const invalidCodes = [
+      `
+        function test() {
+          if (condition) return
+        }
+      `,
+      `
+        function test() {
+          if (condition) { return }
+        }
+      `,
+      `
+        function test() {
+          if (condition) { foo()
+            return
+          }
+        }
+      `,
+      `
+        function test() {
+          if (condition) { m
+            .foo()
+            .bar
+    
+            return
+          }
+        }
+      `,
+      `
+        function test() {
+          if (condition
+            || condition2
+          ) { return }
+        }
+      `,
+    ]
+
+    tester.run(
+      name,
+      noIfInOnelineRule,
+      {
+        valid: validCodes.map(code => ({ code })),
+        invalid: invalidCodes.map(code => ({ code, errors }))
+      }
+    )
+  })
+
+  describe('else branch', () => {
+    const validCodes = [
+      `
+        if (condition) {
+          foo()
+        } else;
+      `,
+      `
+      if (condition) {
+        foo()
+      } else {}
+      `,
+      `
+        function test() {
+          if (condition) {
+            foo()
+          } else {
+            return
+          }
+        }
+      `,
+      `
+        if (condition) {
+          foo()
+        } else {
+          bar()
+          kit()
+        }
+      `,
+      `
+        if (condition) {
+          foo()
+        } else if (condition2) {
+          bar()
+          kit()
+        }
+      `,
+    ]
+    const invalidCodes = [
+      `
+        function test() {
+          if (condition) {
+            foo()
+          } else return
+        }
+      `,
+      `
+        function test() {
+          if (condition) {
+            foo()
+          } else { return }
+        }
+      `,
+      `
+        function test() {
+          if (condition) {
+            foo()
+          } else { bar() 
+            return 
+          }
+        }
+      `,
+      `
+        function test() {
+          if (condition) {
+            foo()
+          } else { m.
+            bar()
+
+            return 
+          }
+        }
+      `,
+    ]
+
+    tester.run(
+      name,
+      noIfInOnelineRule,
+      {
+        valid: validCodes.map(code => ({ code })),
+        invalid: invalidCodes.map(code => ({ code, errors }))
+      }
+    )
+  })
 })
