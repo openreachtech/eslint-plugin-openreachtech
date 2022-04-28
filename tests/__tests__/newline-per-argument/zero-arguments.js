81f62d3a95ebfc6bad2296fe546efc4c369eeff9
Stew Eucen <stew.eucen@gmail.com>
2022-04-29 02:37:17 +0900

    Tidy up test in zero-arguments.

--- tests/__tests__/newline-per-argument/zero-arguments.js
+++ tests/__tests__/newline-per-argument/zero-arguments.js
@@ -7,94 +7,92 @@ const ruleBody = require('../../../lib/newline-per-argument')
 const tester = ESLintHelper.createTester()
 
 describe('newline-per-argument', () => {
-  describe('zero-arguments', () => {
-    const validCases = []
-      .concat([
-        {
-          code: `
-            function method () {
-              return 10
-            }
-          `,
-        },
-        {
-          code: `
-            const method = function () {
-              return 20
-            }
-          `,
-        },
-        {
-          code: `
-            const method = () => {
-              return 30
-            }
-          `,
-        },
-        {
-          code: `
-            class TestClass {
-              constructor () {
-                this.param = 40
-              }
-            }
-          `,
-        },
-        {
-          code: `
-            class TestClass {
-              method () {
-                return 50
-              }
-            }
-          `,
-        },
-        {
-          code: `
-            class TestClass {
-              static method () {
-                return 60
-              }
-            }
-          `,
-        },
-        {
-          code: `
-            const object = {
-              method () {
-                return 70
-              }
-            }
-          `,
-        },
-        {
-          code: `
-            const object = {
-              method: function () {
-                return 80
-              }
-            }
-          `,
-        },
-        {
-          code: `
-            const object = {
-              method: () => {
-                return 90
-              }
-            }
-          `,
-        },
-      ])
-
-    // tester.run([rule name], [rule definition], [test patterns])
-    tester.run(
-      'plane argument',
-      ruleBody,
+  const validCases = []
+    .concat([
       {
-        valid: validCases,
-        invalid: [],
-      }
-    )
-  })
+        code: `
+          function method () {
+            return 10
+          }
+        `,
+      },
+      {
+        code: `
+          const method = function () {
+            return 20
+          }
+        `,
+      },
+      {
+        code: `
+          const method = () => {
+            return 30
+          }
+        `,
+      },
+      {
+        code: `
+          class TestClass {
+            constructor () {
+              this.param = 40
+            }
+          }
+        `,
+      },
+      {
+        code: `
+          class TestClass {
+            method () {
+              return 50
+            }
+          }
+        `,
+      },
+      {
+        code: `
+          class TestClass {
+            static method () {
+              return 60
+            }
+          }
+        `,
+      },
+      {
+        code: `
+          const object = {
+            method () {
+              return 70
+            }
+          }
+        `,
+      },
+      {
+        code: `
+          const object = {
+            method: function () {
+              return 80
+            }
+          }
+        `,
+      },
+      {
+        code: `
+          const object = {
+            method: () => {
+              return 90
+            }
+          }
+        `,
+      },
+    ])
+
+  // tester.run([rule name], [rule definition], [test patterns])
+  tester.run(
+    'zero-arguments',
+    ruleBody,
+    {
+      valid: validCases,
+      invalid: [],
+    }
+  )
 })
