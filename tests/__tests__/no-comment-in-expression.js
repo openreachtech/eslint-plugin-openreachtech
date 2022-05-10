cfe6945d40c3f15a82b1d2a2f549c6f1f8855ccd
lisadhl <907629413@qq.com>
2022-05-10 22:52:50 +0900

    Create no-comment-in-expression rule (for UnaryExpression) _ optimize code again.

--- tests/__tests__/no-comment-in-expression.js
+++ tests/__tests__/no-comment-in-expression.js
@@ -4,9 +4,9 @@
 const ESLintHelper = require('../tools/ESLintHelper')
 const ruleBody = require('../../lib/no-comment-in-expression')
 
-const binaryExpressionCases = require('./no-comment-in-expression_cases/binary-expression_cases')
-const logicalExpressionCases = require('./no-comment-in-expression_cases/logical-expression_cases')
-const unaryExpressionCases = require('./no-comment-in-expression_cases/unary-expression_cases')
+const binaryExpressionCases = require('../no-comment-in-expression_cases/binary-expression_cases')
+const logicalExpressionCases = require('../no-comment-in-expression_cases/logical-expression_cases')
+const unaryExpressionCases = require('../no-comment-in-expression_cases/unary-expression_cases')
 
 const groups = [
   binaryExpressionCases,
